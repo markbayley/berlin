b321e1bc6fcc5af602e248d16614baa5c859b552
markbayley <markyb152@hotmail.com>
2021-07-02 02:43:07 +1000

    issue #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Charts from './components/Charts';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <main>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/charts" component={Charts} />
+                    </Switch>
+                </main>
+            </Router>
+        </div>
+    );
 }
-
 export default App;
---
+++ src/components/Charts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Charts = () => {
+    return (
+        <>
+            <h1>Charts</h1>
+            <p>Charts to go here below</p>
+            <Link to="/">
+                <button>Home</button>
+            </Link>
+        </>
+    );
+};
+
+export default Charts;
