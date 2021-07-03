774c65ea2210065d9d79011d6a04334164ddeafa
markbayley <markyb152@hotmail.com>
2021-07-04 06:21:25 +1000

    TypeScript

--- src/components/ToggleMenu.tsx
+++ src/components/ToggleMenu.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import Tab from '@material-ui/core/Tab';
 
 export default function SimpleMenu() {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -16,8 +22,16 @@ export default function SimpleMenu() {
 
     return (
         <div>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                Open Menu
+            <Button
+                variant="contained"
+                color="primary"
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                size="large"
+            >
+                <MenuIcon />
+                Menu
             </Button>
             <Menu
                 id="simple-menu"
@@ -26,9 +40,22 @@ export default function SimpleMenu() {
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
             >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <Link to="/">
+                        <Tab icon={<HomeIcon />} label="HOME" />
+                    </Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <Link to="/charts">
+                        {' '}
+                        <Tab icon={<BarChartIcon />} label="CHARTS" />
+                    </Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <Link to="/other">
+                        <Tab icon={<PersonPinIcon />} label="OTHER" />
+                    </Link>
+                </MenuItem>
             </Menu>
         </div>
     );
